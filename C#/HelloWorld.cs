8ebba6adc973ab3bf2773cae8223b5adb33ca216
IshaqueR <151510788+IshaqueR@users.noreply.github.com>
2023-11-20 15:05:39 +0200

    Add files via upload

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿
+// The Console.WriteLine() function:
+Console.WriteLine("Exercise 1:");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Hello ");
+Console.WriteLine("World!");
+
+
+// The Console.Write() function:
+Console.WriteLine("Exercise 2:");
+Console.Write("Hello World!");
+Console.Write("Hello ");
+Console.Write("World!");
+
+//Comments are made with two forward slashes.
+
+/*
+ Block comments are made like this
+I can type inside and nothing will be executed by the compiler.
+ */
+
